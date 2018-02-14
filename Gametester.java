7cae73532121bd5ea6922381d02e4f737b56297f
s827361 <729821038@qq.com>
2018-02-13 23:08:37 -0500

    Version 2

--- Gametester.java
+++ Gametester.java
@@ -4,6 +4,8 @@ package eecs1022.eecslab3;
  * Created by user on 2/13/18.
  */
 public class Gametester
+{public static void main(String[] args)
 {
-    Game x1 = new Game ( "m","n","Paper","Scissors");
+    Game x1 = new Game("m", "n", "Paper", "Scissors");
+}
 }
